80449b36be10e1757cf86b99189dc8531318ee7d
arpita-cts <52193489+arpita-cts@users.noreply.github.com>
2019-07-16 14:05:05 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,32 @@ import { AppComponent } from './app.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { ViewTaskComponent } from './components/view-task/view-task.component';
 import { HeaderComponent } from './components/header/header.component';
-import {TaskService} from './task.service'
-import {HttpModule} from '@angular/http'
-import {HttpClientModule} from '@angular/common/http'
-import {FormsModule ,ReactiveFormsModule} from '@angular/forms'
+import {TaskService} from './task.service';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import {TaskFilter} from './task-filter';
+import {UserFilter} from './user-filter';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { AddProjectComponent } from './components/add-project/add-project.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ModalContentComponent } from './components/modal-content/modal-content.component';
+import { ModalProjectComponent } from './components/modal-project/modal-project.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     AddTaskComponent,
     ViewTaskComponent,
-    HeaderComponent
+    HeaderComponent,
+    TaskFilter,
+    UserFilter,
+    AddUserComponent,
+    AddProjectComponent,
+    ModalContentComponent,
+    ModalProjectComponent
   ],
   imports: [
     BrowserModule,
@@ -24,12 +40,15 @@ import {FormsModule ,ReactiveFormsModule} from '@angular/forms'
     HttpModule,
     FormsModule,
     ReactiveFormsModule,
- 
+    NgbModule,
   
   ],
   providers: [
-    TaskService
+    TaskService,
+
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ ModalContentComponent,ModalProjectComponent ]
+
 })
 export class AppModule { }
